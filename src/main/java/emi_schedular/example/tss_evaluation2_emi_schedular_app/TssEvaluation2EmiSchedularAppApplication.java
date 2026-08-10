121560664e22b1dfdc7da3494cf9ded592858f37
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-10 15:00:38 +0530

    app created and database details added

---
+++ src/main/java/emi_schedular/example/tss_evaluation2_emi_schedular_app/TssEvaluation2EmiSchedularAppApplication.java
@@ -0,0 +1,13 @@
+package emi_schedular.example.tss_evaluation2_emi_schedular_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TssEvaluation2EmiSchedularAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TssEvaluation2EmiSchedularAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/emi_schedular/example/tss_evaluation2_emi_schedular_app/TssEvaluation2EmiSchedularAppApplicationTests.java
@@ -0,0 +1,13 @@
+package emi_schedular.example.tss_evaluation2_emi_schedular_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TssEvaluation2EmiSchedularAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
