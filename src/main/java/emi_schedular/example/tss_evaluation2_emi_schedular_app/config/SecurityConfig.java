25abbccad4ec0fb98ef8da8a50e3f0f563b0aef8
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-11 13:16:13 +0530

    add method for password hashing

---
+++ src/main/java/emi_schedular/example/tss_evaluation2_emi_schedular_app/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package emi_schedular.example.tss_evaluation2_emi_schedular_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
