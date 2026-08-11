6ce83f95c85b256cd0f14a8d123d573b95a4bfe0
Dev Bhut <dev.bhut@tssconsultancy.com>
2026-08-11 12:48:19 +0530

    remove nullable

--- src/main/java/emi_schedular/example/tss_evaluation2_emi_schedular_app/entity/User.java
+++ src/main/java/emi_schedular/example/tss_evaluation2_emi_schedular_app/entity/User.java
@@ -27,15 +27,12 @@ public class User extends BaseEntity{
     private String password;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 30)
     private Role role = Role.BORROWER;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 30)
     private UserAccountStatus status = UserAccountStatus.PENDING_VERIFICATION;
 
-    @Column(nullable = false)
-    private Boolean emailVerified = false;
+    private Boolean emailVerified = true;
 
 //    relationships
 
