15ee0ff4c9b186fcd6ba42e99e9dd93cf803cff2
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-16 01:10:11 +0530

    add dtos

---
+++ src/main/java/emi_schedular/example/tss_evaluation2_emi_schedular_app/dto/response/LoanResponseDto.java
@@ -0,0 +1,61 @@
+package emi_schedular.example.tss_evaluation2_emi_schedular_app.dto.response;
+
+import emi_schedular.example.tss_evaluation2_emi_schedular_app.enums.LoanStatus;
+import emi_schedular.example.tss_evaluation2_emi_schedular_app.enums.LoanStrategy;
+import emi_schedular.example.tss_evaluation2_emi_schedular_app.enums.LoanType;
+import emi_schedular.example.tss_evaluation2_emi_schedular_app.enums.RiskLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoanResponseDto {
+
+    private Long id;
+
+    private Long borrowerId;
+
+    private String borrowerName;
+
+    private String borrowerEmail;
+
+    private Long approvedById;
+
+    private String approvedByName;
+
+    private LoanType loanType;
+
+    private BigDecimal requestedAmount;
+
+    private Integer requestedTenure;
+
+    private BigDecimal monthlyIncome;
+
+    private BigDecimal existingMonthlyDebt;
+
+    private BigDecimal dti;
+
+    private RiskLevel riskLevel;
+
+    private LoanStrategy strategy;
+
+    private BigDecimal interestRate;
+
+    private BigDecimal emiAmount;
+
+    private BigDecimal remainingDebtAmount;
+
+    private LoanStatus loanStatus;
+
+    private LocalDateTime approvedAt;
+
+    private LocalDate firstEmiDate;
+}
