6d6a602c8b6b097e62029639eff4bb7a9efdfd37
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-16 01:12:31 +0530

    add methods

---
+++ src/main/java/emi_schedular/example/tss_evaluation2_emi_schedular_app/repository/PaymentRepository.java
@@ -0,0 +1,13 @@
+package emi_schedular.example.tss_evaluation2_emi_schedular_app.repository;
+
+import emi_schedular.example.tss_evaluation2_emi_schedular_app.entity.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Page<Payment> findByEmiLoanId(Long loanId, Pageable pageable);
+}
