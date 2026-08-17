6d49d20bb6c488f28f3f0d967e5f8f459694118e
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-17 17:19:11 +0530

    removed unused import statement

--- src/main/java/emi_schedular/example/tss_evaluation2_emi_schedular_app/entity/AuditLog.java
+++ src/main/java/emi_schedular/example/tss_evaluation2_emi_schedular_app/entity/AuditLog.java
@@ -1,7 +1,6 @@
 package emi_schedular.example.tss_evaluation2_emi_schedular_app.entity;
 
 import emi_schedular.example.tss_evaluation2_emi_schedular_app.enums.AuditAction;
-import emi_schedular.example.tss_evaluation2_emi_schedular_app.enums.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
